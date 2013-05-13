2407db49c0559d2571656f341088af88d200daaf
nswbmw <gxqzk@126.com>
2013-05-13 19:21:59 +0800

    chapter eleven

--- routes/index.js
+++ routes/index.js
@@ -268,6 +268,10 @@ module.exports = function(app){
       res.redirect('back');
     });
   });
+
+  app.all('*', function(req,res){
+    res.render("404");
+  });
 };
 
 function checkLogin(req, res, next){
