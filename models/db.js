1cdb8face71fc69c5296efd9926f3cea84e876c1
nswbmw <gxqzk@126.com>
2013-08-01 15:17:22 +0800

    undo fix #8

--- models/db.js
+++ models/db.js
@@ -2,4 +2,4 @@ var settings = require('../settings'),
     Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}), {safe: true});
+module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}));
