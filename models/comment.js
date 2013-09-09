6174a62d64c58bc8f0c0af1be1ab1d370ad668cd
nswbmw <gxqzk@126.com>
2013-09-09 11:14:25 +0800

    fix 'return' bug

--- models/comment.js
+++ models/comment.js
@@ -18,13 +18,13 @@ Comment.prototype.save = function(callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
-      callback(err);
+      return callback(err);
     }
     //读取 posts 集合
     db.collection('posts', function (err, collection) {
       if (err) {
         mongodb.close();
-        callback(err);
+        return callback(err);
       }
       //通过用户名、时间及标题查找文档，并把一条留言对象添加到该文档的 comments 数组里
       collection.update({
