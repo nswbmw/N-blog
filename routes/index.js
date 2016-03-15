94ac79f69458edae8a7e970aa460c9af08387dcc
nswbmw <gxqzk@126.com>
2016-03-15 22:01:40 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -375,7 +375,7 @@ module.exports = function(app) {
   function checkLogin(req, res, next) {
     if (!req.session.user) {
       req.flash('error', '未登录!'); 
-      res.redirect('/login');
+      return res.redirect('/login');
     }
     next();
   }
@@ -383,7 +383,7 @@ module.exports = function(app) {
   function checkNotLogin(req, res, next) {
     if (req.session.user) {
       req.flash('error', '已登录!'); 
-      res.redirect('back');//返回之前的页面
+      return res.redirect('back');//返回之前的页面
     }
     next();
   }
