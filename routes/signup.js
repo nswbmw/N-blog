094be1595da9e987aad5bdc4943192abccbdc7df
nswbmw <gxqzk@126.com>
2016-12-11 17:24:34 +0800

    Update 4.7 注册.md & fix #248

--- routes/signup.js
+++ routes/signup.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var path = require('path');
 var sha1 = require('sha1');
 var express = require('express');
@@ -41,6 +42,8 @@ router.post('/', checkNotLogin, function(req, res, next) {
       throw new Error('两次输入密码不一致');
     }
   } catch (e) {
+    // 注册失败，异步删除上传的头像
+    fs.unlink(req.files.avatar.path);
     req.flash('error', e.message);
     return res.redirect('/signup');
   }
@@ -70,6 +73,8 @@ router.post('/', checkNotLogin, function(req, res, next) {
       res.redirect('/posts');
     })
     .catch(function (e) {
+      // 注册失败，异步删除上传的头像
+      fs.unlink(req.files.avatar.path);
       // 用户名被占用则跳回注册页，而不是错误页
       if (e.message.match('E11000 duplicate key')) {
         req.flash('error', '用户名已被占用');
