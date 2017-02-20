11033aae5e8d6eb4f59f85b82d74a5992b312d84
nswbmw <gxqzk@126.com>
2017-02-20 11:48:42 +0800

    fix express-session warning

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
   secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
-  resave: true,
-  saveUninitialized: false,
+  resave: true,// 强制更新 session
+  saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
     maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
   },
